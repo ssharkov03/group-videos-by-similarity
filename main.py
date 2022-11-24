7a151acd4752c94fe1a2e4e2a61d443cde08ed3e
SergeyOlymp <serezhasharkov2003@gmail.com>
2022-11-25 01:38:40 +0700

    documentation added

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-
-
 def main():
+    import os
     from meta.meta import MetaData
     from utils.pretty_output import output_prettifier
-    import os
 
     local_save_path = "saved_data/"
     main_bucket_name = 'mid'
     tmp_bucket_name = 'mid-tmp'
-
     logs_path = 'output/'
     target_log_name = 'meta_data_latest.pkl'
     model_path = "model/model_checkpoint/"
@@ -28,13 +25,9 @@ def main():
 if __name__ == '__main__':
     from utils.logger import LOGGING_CONFIG
     from logging.config import dictConfig
+
     # from utils.bug_fixes import fix_download_bug
     # fix_download_bug()
 
-    from utils.pretty_output import output_prettifier
-
-    output_prettifier('output/meta_data_latest.pkl',
-                      'output/pretty_output.txt')
-
     dictConfig(LOGGING_CONFIG)
-    #main()
+    main()
